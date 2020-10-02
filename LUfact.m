273ffa28b226746723e58fa3b802b456ba0d35d2
mlindfelt <71236757+mlindfelt@users.noreply.github.com>
2020-10-01 22:53:25 -0400

    Add files via upload
    ***updated version

--- LUfact.m
+++ LUfact.m
@@ -1,28 +1,37 @@
-function [L,U,p]=LUfact(A)
-[m,n]=size(A);
-L=eye(m,n);
-p=eye(1,m);
-U=A;
+function [L,U,p] = LUfact(A)
+[n,n1] = size(A);   %size of matrix
+L=eye(n,n1);    %initialize L matrix
+w=eye(n,n1);    %initialize w matrix
+p=eye(1,n1);    %initialize p vector
+U=A;            %initialize U matrix
 
-for i=1:n
-    [pivot b]=max(abs(U(i:m,i)));
-    b=b+i-1;
-    if b~=i
-        t1=U(i,:);
-        U(i,:)=U(b,:);
-        U(b,:)=t1;
-        t2=p(i,:);
-        p(i,:)=p(b,:);
-        p(b,:)=t2;
-    if i>=2
-    %    t3=L(i,1:i-1);
-    %    L(i,1:i-1)=L(b,1:i-1);
+if n<n1     %choose iteration variable if non-square matrix
+    a=n;
+else
+    a=n1;
+end
+
+for j=1:a
+  [pivot m] = max(abs(U(j:n, j)));     
+  m=m+j-1;
+  if m~=j   %if unequal
+    U([m,j],:)=U([j,m],:);   %switch rows in U
+    w([m,j],:)=w([j,m],:);   %switch rows in w
+    if j>=2   
+      L([m,j],1:j-1)=L([j,m],1:j-1);   %switch rows in L
+    end
+  end
+  for i=j+1:n      
+    L(i,j)=U(i,j)/U(j,j);   %update L matrix values
+    U(i,:)=U(i,:)-L(i,j)*U(j,:);    %update U matrix values
+  end
+end
+
+v=1;
+for k=1:n       %run through w matrix to create permutation vector
+    for v=1:n1
+        if w(k,v)==1
+           p(k)=v;
+        end
     end
 end
-    for a=i+1:n
-        L(a,i)=U(a,i)/U(i,i);
-        U(a,:)=U(a,:)-L(a,i)*U(i,:);
-    end
-end
-c=1;
-%fprintf('a');
