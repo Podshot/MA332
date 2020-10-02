3cff27a4c0d917c58830b4c0c1f3cc260517f510
Podshot <ben.gothard3@gmail.com>
2020-10-02 14:57:09 -0400

    Extract everything to it's own variable

--- NewtonSys.m
+++ NewtonSys.m
@@ -18,8 +18,12 @@ else
         % df is the jacobian that has the coefficients
         % x0 = x0 - (f(x0)/df(x0)); - not sure if this is what we want
         % exactly but it's roughly what we are looking for
-        x0 = x0 - (feval(f, x0) / feval(df, x0));
-        x0 = x0(:,3);
+        fx = feval(f, x0);
+        dfx = feval(df, x0);
+        xdx = (fx / dfx);
+        xdx = xdx(:,3);
+        x0 = x0 - xdx;
+        %x0 = x0 - (feval(f, x0) / feval(df, x0));
         %x0 = x(mr,1);
         nrmfx = feval(f, x0);
         
