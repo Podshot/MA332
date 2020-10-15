4a64932c88d53b870bacb89027b927c39e956ea9
Podshot <ben.gothard3@gmail.com>
2020-10-15 19:45:20 -0400

    Added Tridiagonal code

---
+++ TriForwardSub.m
@@ -0,0 +1,13 @@
+function [x, status] = TriForwardSub(L, b)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+[n, n1] = size(arg1);
+
+x = b;
+x(1) = b(1);
+for i=2:n
+    x(i) = b(i) - L(i,i-1)*x(i-1);
+end
+
+end
+
---
+++ TriLU.m
@@ -0,0 +1,11 @@
+function [L,U]=TriLU(A, I)
+U = A;
+L = I;
+
+[n,n_] = size(A);
+
+for i=1:(n - 1)
+    L(i+1,i) = U(i+1,i) / U(i,i);
+    U(i+1,i) = U(i+1,i) - L(i+1,i)*U(i,i)
+end
+end
