985dc2d3d58c931d8cbf29700ecfb92198663833
tarynperry <71236795+tarynperry@users.noreply.github.com>
2020-10-18 17:51:09 -0400

    Update TriLU.m

--- TriLU.m
+++ TriLU.m
@@ -1,13 +1,13 @@
-function [L,U]=TriLU(A, I)
-U = A;
-L = I;
+function [L,U]=TriLU(A)
 
-[n,n_] = size(A);
+[n,m] = size(A);
+U = A;
+L = eye(n,m);
 
 for i=1:(n - 1)
     L(i+1,i) = U(i+1,i) / U(i,i);
     U(i+1,i) = U(i+1,i) - L(i+1,i)*U(i,i);
     U(i+1,i+1) = U(i+1,i+1) - L(i+1,i)*U(i,i+1);
-    
 end
+
 end
