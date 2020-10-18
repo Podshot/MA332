2d2caf381f5fe5377cb2566b07c41ca1f33ab1af
tarynperry <71236795+tarynperry@users.noreply.github.com>
2020-10-18 17:50:46 -0400

    Update TriBackwardSub.m

--- TriBackwardSub.m
+++ TriBackwardSub.m
@@ -1,9 +1,14 @@
-function [x, status]=TriBackwardSub(A, b)
-[n,n1]=size(A);
-status = 0;
-x = b;
-for i=(n-1):-1:2
-    x(i) = b(i) - A(i,i-1)*x(i+1);
+function [x, status]=TriBackwardSub(U,y)
+
+[n,m]=size(U);
+
+x = y;
+x(n) = y(n)/U(n,n);
+
+for i= (n-1):-1:1
+    x(i) = (y(i)- U(i,i+1)*x(i+1))/U(i,i);
 end
+
+status = 0;
 
 end
