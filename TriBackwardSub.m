8680c29a532a10cbec99d80360b3b37101097983
mlindfelt <71236757+mlindfelt@users.noreply.github.com>
2020-10-18 22:03:18 -0400

    Added commenting

--- TriBackwardSub.m
+++ TriBackwardSub.m
@@ -1,14 +1,30 @@
 function [x, status]=TriBackwardSub(U,y)
+% Solves the system for x using backward substition on a triangular matrix
 
-[n,m]=size(U);
+% Inputs:
+% U - upper triangular matrix containing the coefficients of the system
+% y - vector containing the right sides of the equations
 
-x = y;
-x(n) = y(n)/U(n,n);
+% Outputs:
+% x - vector of system solutions
+% status - The resulting status of the function call
+%   0: The function returned successfully
+
+% Created on 10/18/2020 by
+% - Haley Braker
+% - Ben Gothard
+% - Madison Lindfelt
+% - Taryn Perry
+
+[n,m]=size(U);  %size of matrix
+
+x = y;                  %initialize x
+x(n) = y(n)/U(n,n);     %update x vector values
 
 for i= (n-1):-1:1
-    x(i) = (y(i)- U(i,i+1)*x(i+1))/U(i,i);
+    x(i) = (y(i)- U(i,i+1)*x(i+1))/U(i,i);      %update x vector values
 end
 
-status = 0;
+status = 0;     %set status to 0
 
 end
