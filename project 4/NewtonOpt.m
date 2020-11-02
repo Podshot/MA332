7374d9f9fad75bbe9754e2bfa1d223195d6e04fd
mlindfelt <71236757+mlindfelt@users.noreply.github.com>
2020-11-01 22:10:07 -0500

    Update NewtonOpt.m

--- project 4/NewtonOpt.m
+++ project 4/NewtonOpt.m
@@ -1,18 +1,40 @@
 function [xbest,fbest,itrcnt,stat] = NewtonOpt(f, Df, Df2, x0, gradTol, xTol, itrBound, s)
-%NEWTONOPT Summary of this function goes here
-%   Detailed explanation goes here
+% Minimizes the function and finds the coefficients for the predicted
+% exponential function
+
+% Inputs:
+% f - function handle to be minimized
+% Df - function handle that evaluates the gradient
+% Df2 - function handle that evaluates the Hessian
+% x0 - initial iterate
+% gradTol - termination criterion
+% xTol - termination criterion
+% itrBound - maximum number of iterations
+% s - line search parameter
+
+% Outputs:
+% xbest - The best x value calculated by minimization by Newton's method
+% fbest - The value of the provided function at xbest
+% itrcnt - number of iterations
+% stat - The status code representing the exit condition of the algorithm
+
+% Created on 10/31/2020 by
+% - Haley Braker
+% - Ben Gothard
+% - Madison Lindfelt
 
 x = x0;
 last_x = 0;
 
 stat = 1;
 
+itrcnt = 1;
 i = 0;
 while i >= itrBound
    function_val = f(x);
    gradient_value = Df(x);
    H = Df2(x);
-   
+
    last_x = x;
    x = x - (gradient_value/H);
    
@@ -29,4 +51,3 @@ end
 xbest = x;
 fbest = f(xbest);
 end
-
