d0b2bc44baad7676caee9698269945cdb4f6a153
Podshot <ben.gothard3@gmail.com>
2020-11-14 15:29:43 -0500

    Updated code for project 5

--- project 5/sirs_model.m
+++ project 5/sirs_model.m
@@ -1,20 +1,36 @@
-beta = 1;
-eps = 0.2;
-yr = 0.1;
-N = 10000;
+clear all
+%beta = 0.45;
+%beta = 1;
+%xi = 0.75;
+%gamma = 1/5;
+beta = .171;
+xi = 0.025;
+gamma = 0.1;
+N = 1000.0;
 
-parameters = [beta, eps, yr, N];
+parameters = [beta, xi, gamma, N];
 
-sim_len = [0, 100];
+sim_len = [0, 400];
 
 y0 = [N - 1, 1, 0];
 
 [t,y] = ode45(@sirs, sim_len, y0, [], parameters);
 plot(t,y);
+legend("Susceptible", "Infectious", "Recovered");
 
 function v = sirs(t, y, params)
     v = zeros(3,1);
-    v(1) = ((-params(1) * y(1) * y(2))/params(4)) + params(2) * y(3);
-    v(2) = ((params(1) * y(1) * y(2))/params(4)) - params(3) * y(2);
-    v(3) = params(3) * y(2) - params(2) * y(3);
+    
+    S = y(1);
+    I = y(2);
+    R = y(3);
+    
+    b = params(1);
+    x_x = params(2);
+    g = params(3);
+    n = params(4);
+    
+    v(1) = ((-b * S * I) / n) + (x_x * R);
+    v(2) = ((b * S * I) / n) - (g * I);
+    v(3) = (g * I) - (x_x * R);
 end
